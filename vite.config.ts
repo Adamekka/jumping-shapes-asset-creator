8006e2ffb55d177f1fe6df28d9c581fea4302bd7
Adam Cvikl <adam.wasd4@gmail.com>
2023-07-02 16:49:35 +0200

    enable deploying to `gh-pages`

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    base: `/jumping-shapes-asset-creator/`,
     plugins: [sveltekit()]
 });
